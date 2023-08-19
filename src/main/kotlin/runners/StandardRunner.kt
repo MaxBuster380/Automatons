ac1286f69e39ae845e568ac8555003e78ef22eb5
MaxBuster <maxbuster380@gmail.com>
2023-08-19 10:18:50 +0200

    Added StandardRunner.canApply

--- src/main/kotlin/runners/StandardRunner.kt
+++ src/main/kotlin/runners/StandardRunner.kt
@@ -12,23 +12,32 @@ class StandardRunner(private val automaton : Automaton, startStates : Set<State>
 		currentStates = startStates
 	}
 
+	// PUBLIC INSTANCE METHODS
+
 	/**
 	 * Changes the current states by derivating with an event.
 	 * @param event Event to derivate with.
 	 * @throws NoStateException when the runner has no current states after derivating.
 	 */
 	fun apply(event : Event) {
-		val newStates = mutableSetOf<State>()
-
-		for(startState in currentStates) {
-			newStates.addAll(automaton.derivate(startState, event))
-		}
+		val newStates = calculateStatesDerivate(event)
 
 		if (newStates.isEmpty()) {
 			throw NoStateException("This runner has no more current states.")
 		}
 
 		currentStates = newStates
+	}
+
+	/**
+	 * Checks if changing the current states by an event leads to an error.
+	 * @param event Event to test on.
+	 * @return True if using apply(event) throws a NoStateException.
+	 */
+	fun canApply(event : Event) : Boolean {
+		val newStates = calculateStatesDerivate(event)
+
+		return newStates.isNotEmpty()
 	}
 
 	/**
@@ -45,5 +54,17 @@ class StandardRunner(private val automaton : Automaton, startStates : Set<State>
 	 */
 	fun getAutomaton() : Automaton {
 		return automaton
+	}
+
+	// PRIVATE INSTANCE METHODS
+
+	private fun calculateStatesDerivate(event : Event) : Set<State> {
+		val res = mutableSetOf<State>()
+
+		for(startState in currentStates) {
+			res.addAll(automaton.derivate(startState, event))
+		}
+
+		return res
 	}
 }
