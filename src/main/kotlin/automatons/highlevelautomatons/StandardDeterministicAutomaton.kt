b86cfd426def9d10bca25688eaf19c04e8db8740
MaxBuster <maxbuster380@gmail.com>
2023-08-25 11:13:24 +0200

    Made StandardDeter.Automaton use DeterministicAutomaton

--- src/main/kotlin/automatons/highlevelautomatons/StandardDeterministicAutomaton.kt
+++ src/main/kotlin/automatons/highlevelautomatons/StandardDeterministicAutomaton.kt
@@ -1,32 +1,26 @@
 package automatons.highlevelautomatons
 
 import automatons.Automaton
+import automatons.DeterministicAutomaton
 import automatons.StandardAutomaton
 import events.Event
 import exceptions.DuplicateTransitionException
 import states.State
 
 /**
- * A deterministic automaton is an automaton that can't be in multiple states at a time.
- * This implementation will disable having multiple transitions with equal start state and event.
+ * Basic, unoptimized implementation of DeterministicAutomaton.
+ * @see DeterministicAutomaton
  */
 class StandardDeterministicAutomaton(
 	private val subAutomaton : Automaton
-) : Automaton {
+) : DeterministicAutomaton {
 	constructor() : this(StandardAutomaton())
 
 	// PUBLIC INSTANCE METHODS
 
-	/**
-	 * Finds the state inserted in relation to a given start state and an event.
-	 * Pattern : (Start State, Event) -> (End State)
-	 * This is a shortcut method for derivate(), that outputs a state instead of a set of states.
-	 * @param startState State to search with, as start state.
-	 * @param event Event to search with.
-	 * @return The state with a transition defined with the given start state and the given event, null if no such state exists.
-	 * @see derivate
-	 */
-	fun derivateSingle(startState: State, event: Event): State? {
+	// PUBLIC INSTANCE METHODS - INTERFACE DeterministicAutomaton
+
+	override fun derivateSingle(startState: State, event: Event): State? {
 		return try {
 			subAutomaton.derivate(startState, event).first()
 		}catch(_:Exception) {
@@ -35,15 +29,7 @@ class StandardDeterministicAutomaton(
 	}
 
 	// PUBLIC INSTANCE METHODS - INTERFACE Automaton
-
-	/**
-	 * Adds a transition relationship between a start state, an event, and an end state.
-	 * @param startState Start state of the new transition.
-	 * @param event Event of the new transition.
-	 * @param endState End state of the new transition.
-	 * @return This instance of Automaton for successive calls.
-	 * @throws DuplicateTransitionException when (startState, event) is already defined.
-	 */
+	
 	override fun add(startState: State, event: Event, endState: State): Automaton {
 		val transitionAlreadyDefined = subAutomaton.derivate(startState, event).isNotEmpty()
 		if (!transitionAlreadyDefined) {
